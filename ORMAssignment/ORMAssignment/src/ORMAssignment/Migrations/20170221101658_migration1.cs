c235ac98ad3cf7557f6060d9d22cc517a08116bc
Ritika Mohata <ritika@promact.com>
2017-02-21 17:15:01 +0530

    ORM Assignment Added

---
+++ ORMAssignment/ORMAssignment/src/ORMAssignment/Migrations/20170221101658_migration1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ORMAssignment.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_product_update_UpdateModelId",
+                table: "product");
+
+            migrationBuilder.DropIndex(
+                name: "IX_product_UpdateModelId",
+                table: "product");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateModelId",
+                table: "product");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "update",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_update_ProductId",
+                table: "update",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_update_product_ProductId",
+                table: "update",
+                column: "ProductId",
+                principalTable: "product",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_update_product_ProductId",
+                table: "update");
+
+            migrationBuilder.DropIndex(
+                name: "IX_update_ProductId",
+                table: "update");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "update");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdateModelId",
+                table: "product",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_UpdateModelId",
+                table: "product",
+                column: "UpdateModelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_product_update_UpdateModelId",
+                table: "product",
+                column: "UpdateModelId",
+                principalTable: "update",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ ORMAssignment/ORMAssignment/src/ORMAssignment/Program.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ORMAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new DataBaseConnection())
+            {
+                db.Database.Migrate();
+                db.update.Add(new UpdateModel
+                {
+                    Name = "abc",
+                    UpdateDescription = "abc produdct updated",
+                    ProductId = 19
+
+                });
+
+                db.update.Add(new UpdateModel
+                {
+                    Name = "def",
+                    UpdateDescription = "def product updated",
+                    ProductId = 20
+                });
+
+                db.update.Add(new UpdateModel
+                {
+                    Name = "xyz",
+                    UpdateDescription = "xyz product updated",
+                    ProductId = 22
+                });
+
+                db.update.Add(new UpdateModel
+                {
+                    Name = "phones",
+                    UpdateDescription = "phones product updated",
+                    ProductId = 20
+                });
+
+                db.product.Add(new ProductModel
+                {
+                    ProductName = "phones",
+                    HomePageUrl = "http:\\phones.com",
+                });
+
+                db.product.Add(new ProductModel
+                {
+                    ProductName = "headphones",
+                    HomePageUrl = "http:\\headphones.com",
+
+                });
+                db.product.Add(new ProductModel
+                {
+                    ProductName = "back covers",
+                    HomePageUrl = "http:\\covers.com",
+                });
+                db.product.Add(new ProductModel
+                {
+                    ProductName = "abc",
+                    HomePageUrl = "http:\\abc.com",
+                });
+
+                db.product.Add(new ProductModel
+                {
+                    ProductName = "def",
+                    HomePageUrl = "http:\\def.com",
+                });
+
+                db.product.Add(new ProductModel
+                {
+                    ProductName = "xyz",
+                    HomePageUrl = "http:\\xyz.com",
+                });
+
+                db.update.Remove(new UpdateModel
+                {
+                    Id = 24
+                });
+
+                db.update.Update(new UpdateModel
+                {
+                    Id = 17,
+                    ProductId = 20,
+                    Name = "covers",
+                    UpdateDescription = "def updated to covers",
+                });
+
+                db.product.Update(new ProductModel
+                {
+                    ProductId = 20,
+                    ProductName = "tempered glass",
+                    HomePageUrl = "http:\\temperedglass.com",
+                });
+
+                db.product.Remove(new ProductModel
+                {
+                    ProductId = 19
+                });
+
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} changes saved", count);
+                Console.WriteLine();
+            }
+        }
+    }
+}
