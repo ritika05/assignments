cb626556fbdac5ecfe20df3a9238c3bec433cf71
Ritika Mohata <ritika@promact.com>
2017-03-08 08:39:58 +0530

    ORM Assignment added

---
+++ ORMAssignment/ORMAssignment/src/ORMAssignment/Migrations/20170221083934_111.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ORMAssignment.Migrations
+{
+    public partial class _111 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    userId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    dateOfBirth = table.Column<string>(nullable: true),
+                    emailId = table.Column<string>(nullable: false),
+                    frstName = table.Column<string>(maxLength: 50, nullable: false),
+                    lastName = table.Column<string>(nullable: true),
+                    url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.userId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "product",
+                columns: table => new
+                {
+                    productId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserModeluserId = table.Column<int>(nullable: true),
+                    description = table.Column<string>(nullable: true),
+                    homePageUrl = table.Column<string>(nullable: false),
+                    productName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_product", x => x.productId);
+                    table.ForeignKey(
+                        name: "FK_product_user_UserModeluserId",
+                        column: x => x.UserModeluserId,
+                        principalTable: "user",
+                        principalColumn: "userId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_product_UserModeluserId",
+                table: "product",
+                column: "UserModeluserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "product");
+
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
---
+++ ORMAssignment/ORMAssignment/src/ORMAssignment/UpdateModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ORMAssignment
+{
+    public class UpdateModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public String Name { get; set; }
+
+        [Required]
+        public String UpdateDescription { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public ProductModel product { get; set; }
+    }
+}
